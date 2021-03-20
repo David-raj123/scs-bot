302bb74c3cabc768a1192df2a6ea7ce3bf519924
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-03-20 21:51:27 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,13 +3,13 @@ from pathlib import Path
 
 class Config:
     
-    API_ID = int(os.environ.get('1543212'))
-    API_HASH = os.environ.get('d47de4b25ddf79a08127b433de32dc84')
-    BOT_TOKEN = os.environ.get('1780099168:AAHlhBygBSWnP39Ok8ycUqeNPjcP635SH4Y')
-    SESSION_NAME = os.environ.get('@TheScs_Robot')
-    LOG_CHANNEL = int(os.environ.get('-1001376476949'))
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split('1478357602')]
+    API_ID = int(os.environ.get("1543212"))
+    API_HASH = os.environ.get("d47de4b25ddf79a08127b433de32dc84")
+    BOT_TOKEN = os.environ.get("1780099168:AAHlhBygBSWnP39Ok8ycUqeNPjcP635SH4Y")
+    SESSION_NAME = os.environ.get("@TheScs_Robot")
+    LOG_CHANNEL = int(os.environ.get("-1001376476949"))
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split("1478357602")]
     MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 2))
     MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 600))
     TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
